02e9a87cc3d6ab6df9943f3567f17ba5237aa283
Kiran Eiden <kiran_eiden@berkeley.edu>
2022-06-30 10:14:50 -0600

    Ostensibly working PeleLMeX setup with neural net

--- Exec/RegTests/FlameSheet/get_pmfdat_and_ctable.py
+++ Exec/RegTests/FlameSheet/get_pmfdat_and_ctable.py
@@ -1,7 +1,7 @@
 import cantera as ct
 import numpy as np
 import pandas as pd
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 import ctable_tools as ctable
 
 # Thermo Conditions
@@ -10,11 +10,71 @@ temp = 298.0
 phi = 1.0
 fuel = 'CH4'
 oxid = 'O2:1.0, N2:3.76'
-mechanism = 'drm19.cti'
+mechanism = 'drm19.yaml'
 trans = 'Mix'
 progvars = ["CO2","H2O","CO","H2"]
 ctable_specs = ["CO2","H2O","CO","H2","N2","O2","OH"]
 outfile_prefix = 'prem_drm19_phi1_p1_t298'
+# net_info_file = None
+net_info_file = "cmlm_net_info.txt"
+
+def load_info_file(filename):
+    """Load neural net info file."""
+    
+    with open(filename, 'r') as file:
+        umap = defaultdict(list)
+        line_cont = False
+        for line in file:
+            try:
+                com_idx = line.index("#")
+            except:
+                com_idx = None
+            if com_idx is not None:
+                line = line[:com_idx].strip()
+                if (not line): continue
+            if not line_cont:
+                varname, value = line.split(' = ')
+            else:
+                value = line
+            umap[varname.strip()] += value.strip().rstrip("\\").split()
+            line_cont = line.strip().endswith("\\")
+    
+    dimnames = umap["dimnames"]
+    defn0 = "def_" + dimnames[0]
+    coeff = np.zeros((len(dimnames), len(umap[defn0])), dtype=np.float32)
+    varidx = np.zeros((len(dimnames), len(umap[defn0])), dtype=np.int32)
+    srcidx = np.zeros((len(dimnames), len(umap[defn0])), dtype=np.int32)
+    combmap = dict()
+    
+    for i in range(len(dimnames)):
+        defn = "def_" + dimnames[i]
+        for j, item in enumerate(umap[defn]):
+            
+            c, v = item.split("*")
+            c = c.strip()
+            v = v.strip()
+            
+            coeff[i,j] = float(c)
+            varidx[i,j] = umap["varnames"].index(v)
+            combmap[v] = j
+            
+            if v.startswith("Y-"):
+                v = v[2:]
+            try:
+                idx = umap["varnames"].index("SRC_" + v)
+            except ValueError:
+                idx = -1
+            srcidx[i,j] = idx
+    
+    manibiases = np.fromiter(map(float, umap["manibiases"]), dtype=np.float32)
+    return umap, coeff, varidx, srcidx, manibiases, combmap
+    
+# Read info file if one is provided
+if net_info_file is not None:
+    umap, coeff, varidx, srcidx, manibiases, combmap = load_info_file(net_info_file)
+else:
+    coeff, varidx, srcidx, manibiases, combmap = (None,)*5
+    umap = defaultdict(list)
 
 # Flame Numerics
 width = 0.1
@@ -28,6 +88,7 @@ max_points = 10000
 # Set up the flame
 gas = ct.Solution(mechanism)
 gas.set_equivalence_ratio(phi, fuel, oxid)
+gas.TP = temp, press
 flame = ct.FreeFlame(gas, width=width)
 flame.set_refine_criteria(ratio=ratio, slope=slope, curve=curve, prune=prune)
 flame.set_max_grid_points(1,max_points)
@@ -64,11 +125,21 @@ data["SRC_PROG"] = specRRdata[progvars].sum(axis=1)
 data.loc[0,"SRC_PROG"] = 0.0
 data.loc[len(flame.grid)-1,"SRC_PROG"] = 0.0
 
+# Compute manifold variable(s) for net
+for i, dim in enumerate(umap["dimnames"]):
+    data[dim] = 0.0
+    for k in combmap:
+        if k.startswith('Y-'):
+            k_slc = k[2:]
+        else:
+            k_slc = k
+        data[dim] += coeff[i, combmap[k]] * specYdata[k_slc]
+
 # Convert to CGS units
 ctable.convert_chemtable_units(data)
 
 # Make the chemtable
-chemtable = data.drop(columns=["VEL","X","PROG"])
+chemtable = data.drop(columns=(["VEL","X","PROG"]+umap["dimnames"]))
 for spec in ctable_specs:
     chemtable["Y-"+spec] = specYdata[spec]
 chemtable = pd.DataFrame(chemtable.values,
@@ -101,6 +172,9 @@ rename = {'VEL':'u', 'T':'temp','RHO':'rho'}
 keepvars = ["X","T","VEL","RHO"]
 df = data[keepvars].rename(columns=rename)
 df2 = pd.DataFrame(data[["PROG","RHO"]].values, columns=['X0','XRHO'])
+df3 = pd.DataFrame(data[umap["dimnames"]+["RHO"]].values,
+        columns=([f"X{i}" for i in range(len(umap["dimnames"]))]+['XRHO']))
 
 write_dat_file(outfile_prefix+'.dat', pd.concat([df,specXdata],axis=1))
 write_dat_file(outfile_prefix+'_mani.dat', pd.concat([df,df2],axis=1))
+write_dat_file(outfile_prefix+'_mani_nn.dat', pd.concat([df,df3],axis=1))
