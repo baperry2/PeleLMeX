fac91babb2234a4f00feb001779225ad8c02dd93
Bruce Perry <bruce.perry@nrel.gov>
2025-02-13 16:15:18 -0700

    simplify sCO2 case as much as possible

--- Exec/Production/sCO2combustor/pelelmex_prob.cpp
+++ Exec/Production/sCO2combustor/pelelmex_prob.cpp
@@ -6,14 +6,9 @@ PeleLM::readProbParm() // NOLINT(readability-make-member-function-const)
 {
   amrex::ParmParse pp("prob");
 
-  pp.query("T_mean", prob_parm->T_mean);
+  pp.query("T_0", prob_parm->T_0);
   pp.query("P_mean", prob_parm->P_mean);
-  pp.query("Y_fuel", prob_parm->Y_fuel);
-  pp.query("Y_oxid", prob_parm->Y_o2);
-  pp.query("T_hot", prob_parm->T_hot);
-  pp.query("T_wall", prob_parm->Twall);
-  pp.query("meanFlowMag", prob_parm->meanFlowMag);
-  pp.query("EBinflow_T", prob_parm->EBinflow_T);
-  pp.query("EBinflow_vel", prob_parm->EBinflow_vel);
-  pp.query("EBinflow_Yfuel", prob_parm->EBinflow_Yfuel);
+  pp.query("T_in", prob_parm->T_in);
+  pp.query("u_in", prob_parm->u_in);
+  pp.query("u_0", prob_parm->u_0);
 }
