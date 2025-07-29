c870deaa08e73980c7374f9d4d9759b3040b5d53
Bruce Perry <bruce.perry@nrel.gov>
2025-07-29 13:54:57 -0600

    exmaple adding a tablke lookup to flamesheet case

--- Exec/RegTests/FlameSheet/pelelmex_prob.cpp
+++ Exec/RegTests/FlameSheet/pelelmex_prob.cpp
@@ -13,10 +13,18 @@ PeleLM::readProbParm()
 
   PeleLM::prob_parm->eosparm = PeleLM::eos_parms.device_parm();
   PeleLM::pmf_data.initialize();
+  PeleLM::prob_parm->tabfunc_par = new pele::physics::TabulatedFunctionParams();
+  PeleLM::prob_parm->tabfunc_par->host_only_parm().parm_parse_prefix = "test";
+  PeleLM::prob_parm->tabfunc_par->initialize();
+  PeleLM::prob_parm->tabfunc_dat = static_cast<const pele::physics::TabulatedFunctionData*>(PeleLM::prob_parm->tabfunc_par->device_parm());
+  const std::string varname = "T";
+  PeleLM::prob_parm->idxT = pele::physics::get_var_index(varname.c_str(), &PeleLM::prob_parm->tabfunc_par->host_parm());
 }
 
 void
 PeleLM::freeProbParm()
 {
   PeleLM::pmf_data.deallocate();
+  PeleLM::prob_parm->tabfunc_par->deallocate();
+  delete PeleLM::prob_parm->tabfunc_par;
 }
