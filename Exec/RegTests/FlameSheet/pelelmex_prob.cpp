7b23c7c428c2cafafd16f874db50338fa55e1644
Bruce Perry <bruce.perry@nrel.gov>
2026-03-13 15:55:34 -0600

    modified flamesheet that has ability to look up from a ctb file

--- Exec/RegTests/FlameSheet/pelelmex_prob.cpp
+++ Exec/RegTests/FlameSheet/pelelmex_prob.cpp
@@ -17,10 +17,25 @@ PeleLM::readProbParm()
 
   PeleLM::prob_parm->eosparm = PeleLM::eos_parms.device_parm();
   PeleLM::pmf_data.initialize();
+
+  PeleLM::prob_parm->tabfunc_parms = new pele::physics::TabulatedFunctionParams;
+  std::string bc_data_prefix;
+  pp.get("bc_data_prefix", bc_data_prefix);
+  PeleLM::prob_parm->tabfunc_parms->host_only_parm().parm_parse_prefix = bc_data_prefix;
+  PeleLM::prob_parm->tabfunc_parms->initialize();
+  PeleLM::prob_parm->tabfunc_data_d =  static_cast<const pele::physics::TabulatedFunctionData*>(PeleLM::prob_parm->tabfunc_parms->device_parm());
+  const bool required = true;
+  const auto host_parm = &PeleLM::prob_parm->tabfunc_parms->host_parm();
+  PeleLM::prob_parm->idx_out_zmix = get_var_index("mean-f", host_parm, required);
+  PeleLM::prob_parm->idx_out_xvel = get_var_index("mean-x-velocity", host_parm, required);
+  PeleLM::prob_parm->idx_out_yvel = get_var_index("mean-y-velocity", host_parm, required);
+  PeleLM::prob_parm->idx_out_zvel = get_var_index("mean-z-velocity", host_parm, required); 
 }
 
 void
 PeleLM::freeProbParm()
 {
   PeleLM::pmf_data.deallocate();
+  PeleLM::prob_parm->tabfunc_parms->deallocate();
+  delete PeleLM::prob_parm->tabfunc_parms;
 }
